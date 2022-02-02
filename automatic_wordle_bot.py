f41e53e3a91fe823f2db66e43990ca60072eed84
motloch <18506389+motloch@users.noreply.github.com>
2022-02-01 20:11:35 -0500

    Polishing

--- automatic_wordle_bot.py
+++ automatic_wordle_bot.py
@@ -1,3 +1,6 @@
+"""
+Script that uses selenium to solve today's Wordle challenge
+"""
 from selenium.webdriver import Chrome
 from website_interactions import initial_click, get_key_buttons, get_result
 import time
@@ -10,11 +13,12 @@ from constants import NCHAR, ORD_A, DELAY
 driver = Chrome()
 driver.get("https://www.powerlanguage.co.uk/wordle/")
 
-# Get rid of the help, load button elements
+# Get rid of the help element blocking the view
 initial_click(driver)
+# Get button elements representing the keyboard
 buttons = get_key_buttons(driver)
 
-# Load allowed words / solutions as arrays of integers. Get their numbers.
+# Load allowed words / solutions as arrays of integers. Get their cardinality.
 wlist = load_words_as_array_of_int('wordlist.txt')
 NW = len(wlist)
 solns = load_words_as_array_of_int('solutions.txt')
@@ -30,23 +34,28 @@ for i, sol in enumerate(solns):
 guess = 'roate'
 # In the form of an int array, which is the representation of words we have been using.
 guess_arr = [ord(x) - ORD_A for x in guess]
-line_no = 1
+# We are on our first guess
+guess_no = 1
 
 not_solved = True
 
 # Continue until we have only one solution left
 while not_solved:
 
+    # Use selenium to enter the guess onto the webpage
     print('Guessing: ', guess)
     for c in guess:
         buttons[c].click()
     buttons['enter'].click()
 
+    # Wait for the graphic to pass
     time.sleep(DELAY)
 
-    result = get_result(driver, line_no)
+    # Read off the result (example: gyxxy)
+    result = get_result(driver, guess_no)
     print('Result: ', result)
-    line_no += 1
+
+    guess_no += 1
     if result == 'ggggg':
         not_solved = False
 
@@ -55,11 +64,13 @@ while not_solved:
     solns = solns[filt] 
     letters_present = letters_present[filt]
 
+    # Pick the remaining solution, or find the next best guess
     if len(solns) == 1:
         guess_arr = solns[0]
         guess = ''.join([chr(ORD_A + c) for c in solns[0]])
     else:
         guess, guess_arr = find_next_guess(wlist, solns, letters_present)
 
+# Output the final solution one more time
 solution = ''.join([chr(ORD_A + c) for c in solns[0]]).upper()
 print('\nSolution is: ', solution)
